0deab7c4ea43d82961b6c636c6ea775e9abd1494
Christof Vollrath <christof@taobits.net>
2016-08-02 23:18:51 +0200

    Initial commit

---
+++ app/src/main/java/net/taobits/androidstudiotest/AndroidStudioTestMainActivity.java
@@ -0,0 +1,13 @@
+package net.taobits.androidstudiotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidStudioTestMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_studio_test_main);
+    }
+}
