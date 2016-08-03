2c72c2beadf6fb720cf3df1c15fd45ed40deeba7
Christof Vollrath <christof@taobits.net>
2016-08-03 23:22:43 +0200

    Now we have menu

--- app/src/main/java/net/taobits/androidstudiotest/AndroidStudioTestMainActivity.java
+++ app/src/main/java/net/taobits/androidstudiotest/AndroidStudioTestMainActivity.java
@@ -2,6 +2,8 @@ package net.taobits.androidstudiotest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class AndroidStudioTestMainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class AndroidStudioTestMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_studio_test_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_android_studio_test, menu);
+        return true;
     }
 }
